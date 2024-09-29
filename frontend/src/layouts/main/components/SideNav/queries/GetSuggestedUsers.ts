6b1b405a2a47d972260b1bda1a1555297c239744
Nikitapil <nikitapiliavets@gmail.com>
2024-09-29 16:45:06 +0300

    add limit to get suggested users

--- frontend/src/layouts/main/components/SideNav/queries/GetSuggestedUsers.ts
+++ frontend/src/layouts/main/components/SideNav/queries/GetSuggestedUsers.ts
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client';
 
 export const GET_SUGGESTED_USERS = gql`
   query GetSuggestedUsers {
-    getUsers {
+    getUsers(getUsersInput: { take: 5 }) {
       id
       fullname
       email

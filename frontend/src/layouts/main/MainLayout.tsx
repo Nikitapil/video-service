0a462b58ef4f3d6c33afc63b144331bda25c3dca
Nikitapil <nikitapiliavets@gmail.com>
2024-09-30 20:56:48 +0300

    refactor MainLayout component

--- frontend/src/layouts/main/MainLayout.tsx
+++ frontend/src/layouts/main/MainLayout.tsx
@@ -2,14 +2,18 @@ import { ReactNode } from 'react';
 import AppHeader from '../components/AppHeader/AppHeader.tsx';
 import SideNav from './components/SideNav/SideNav.tsx';
 
-const MainLayout = ({ children }: { children: ReactNode }) => {
+interface MainLayoutProps {
+  children: ReactNode;
+}
+
+const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <div>
       <AppHeader />
 
       <div className="container mx-auto flex justify-between px-0 lg:px-2.5">
         <SideNav />
-        <div className="ml-[55px] mt-[70px] flex-1 px-3 sm:px-6 lg:ml-[310px]">{children}</div>
+        <main className="ml-14 mt-16 flex-1 px-3 sm:px-6 lg:ml-80">{children}</main>
       </div>
     </div>
   );

7f60569132a8933f057ae2aebcf522108ab36721
Nikitapil <nikitapiliavets@gmail.com>
2024-10-24 21:51:12 +0300

    refactor CreateComment mutations

--- frontend/src/modules/post/mutations/CreateComment.ts
+++ frontend/src/modules/post/mutations/CreateComment.ts
@@ -1,21 +1,9 @@
-import {gql} from "@apollo/client";
+import { gql } from '@apollo/client';
 
 export const CREATE_COMMENT = gql`
   mutation CreateComment($text: String!, $postId: Int!) {
-      createComment(text: $text, postId: $postId) {
-          text
-          id
-          createdAt
-          user {
-              id
-              fullname
-              email
-          }
-          post {
-              id
-              text
-              video
-          }
-      }
+    createComment(text: $text, postId: $postId) {
+      id
+    }
   }
-`
+`;

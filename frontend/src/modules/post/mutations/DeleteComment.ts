8eff71f40582cc91c7b3bcc62a8d24385ebf6697
Nikitapil <nikitapiliavets@gmail.com>
2024-10-24 21:55:54 +0300

    refactor DeleteComment mutations

--- frontend/src/modules/post/mutations/DeleteComment.ts
+++ frontend/src/modules/post/mutations/DeleteComment.ts
@@ -1,7 +1,7 @@
-import {gql} from "@apollo/client";
+import { gql } from '@apollo/client';
 
 export const DELETE_COMMENT = gql`
-    mutation DeleteComment($id: Int!) {
-        deleteComment(id: $id)
-    }
-`
+  mutation DeleteComment($id: Int!) {
+    deleteComment(id: $id)
+  }
+`;

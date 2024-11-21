53f4b9dc6b5483bd80765d191ef632d48a489d13
Nikitapil <nikitapiliavets@gmail.com>
2024-11-21 21:52:46 +0300

    remove useless ErrorType

--- backend/src/auth/types/LoginResponse.ts
+++ backend/src/auth/types/LoginResponse.ts
@@ -1,12 +1,8 @@
 import { Field, ObjectType } from '@nestjs/graphql';
-import { ErrorType } from './ErrorType';
 import { User } from '../../user/types/user.type';
 
 @ObjectType()
 export class LoginResponse {
   @Field(() => User, { nullable: true })
   user: User;
-
-  @Field(() => ErrorType, { nullable: true })
-  error?: ErrorType;
 }

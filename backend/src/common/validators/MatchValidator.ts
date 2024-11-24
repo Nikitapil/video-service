46aad46819b1ffc58287dfe35cd6141ae25db966
Nikitapil <nikitapiliavets@gmail.com>
2024-11-24 22:04:16 +0300

    refactor MatchValidator

--- backend/src/common/validators/MatchValidator.ts
+++ backend/src/common/validators/MatchValidator.ts
@@ -8,9 +8,12 @@ export function IsMatch(
   property: string,
   validationOptions?: ValidationOptions
 ) {
-  return function (object: Record<string, any>, propertyName: string) {
+  return function (
+    object: Record<string, any>,
+    propertyName: keyof typeof object
+  ) {
     registerDecorator({
-      name: 'isLongerThan',
+      name: 'IsMatch',
       target: object.constructor,
       propertyName: propertyName,
       constraints: [property],
@@ -18,7 +21,7 @@ export function IsMatch(
       validator: {
         validate(value: any, args: ValidationArguments) {
           const [relatedPropertyName] = args.constraints;
-          const relatedValue = (args.object as any)[relatedPropertyName];
+          const relatedValue = args.object[relatedPropertyName];
           return value === relatedValue;
         }
       }

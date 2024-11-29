81854cb7489afeab05f461c88b43507043a34938
Nikitapil <nikitapiliavets@gmail.com>
2024-11-29 22:31:34 +0300

    refactor post resolver

--- backend/src/post/post.resolver.ts
+++ backend/src/post/post.resolver.ts
@@ -1,7 +1,6 @@
-import { Args, Context, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { PostService } from './post.service';
 import { PostType } from './types/post.type';
-import { Request } from 'express';
 import { UseGuards } from '@nestjs/common';
 import { GraphQLAuthGuard } from '../auth/guards/graphql-auth.guard';
 import { CreatePostDto } from './dto/create-post.dto';
@@ -46,17 +45,17 @@ export class PostResolver {
 
   @Mutation(() => SuccessMessageType)
   async deletePost(
-    @Context() context: { req: Request },
+    @User() user: TokenUserDto,
     @Args('id', { type: () => Int }) id: number
   ): Promise<SuccessMessageType> {
-    return this.postService.deletePost(id, context.req.user.sub);
+    return this.postService.deletePost(id, user.sub);
   }
 
   @Query(() => [PostType])
   async getFavoriteUserPosts(
     @Args('userId', { type: () => Int }) userId: number,
-    @Context() context: { req: Request }
+    @User() user: TokenUserDto
   ): Promise<PostType[]> {
-    return this.postService.getFavoriteUserPosts(userId, context.req.user.sub);
+    return this.postService.getFavoriteUserPosts(userId, user.sub);
   }
 }
